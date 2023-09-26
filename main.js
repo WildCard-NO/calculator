6a73f65b9ab313ef28b8fee24c976e566e397178
WildCard-NO <kalex.boe@gmail.com>
2023-09-26 09:45:21 +0200

    arithmetic

---
+++ main.js
@@ -0,0 +1,4 @@
+const addButton = document.getElementById('add');
+const subtractButton = document.getElementById('subtract');
+const multiplyButton = document.getElementById('multiply');
+const divideButton = document.getElementById('divide');
